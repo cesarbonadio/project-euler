c031c55e0fe5c50bfc1e8e803f184046b58a90bd
César Bonadío <32617164+cesarbonadio@users.noreply.github.com>
2018-07-12 22:42:49 -0400

    optimizar 7

--- src/7.cpp
+++ src/7.cpp
@@ -4,14 +4,20 @@
 using namespace std;
 
 
-int primo(int x) {
-	int i,cont=0;
+int isprime(int x) {
+	if (x <= 1) return 0;
 
-	for (i = 1; i <= x; i++)
-		if (x%i == 0) cont++;
+	else if (x <= 3) return 1;
 
-	if (cont > 2) return 0;
-	else return 1;
+	else if ((x % 2 == 0) || (x % 3 == 0)) return 0;
+
+	int i = 5;
+	while (i * i <= x) {
+		if ((x % i == 0) || (x % (i + 2) == 0)) return 0;
+		i = i + 6;
+	}
+
+	return 1;
 }
 
 
@@ -19,9 +25,10 @@ int main() {
 
 	long int i = 0; int cont = 0;
 
-	while (cont <= 10001) { 
+	while (cont <= 10001) {
 		i++;
-		if (primo(i)) cont++;}
+		if (isprime(i)) cont++;
+	}
 
 	cout << i << endl;
 
