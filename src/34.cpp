5e8e31240de61bedf3ef7f22c520d9ea307ae121
César Bonadío <32617164+cesarbonadio@users.noreply.github.com>
2018-07-14 19:12:58 -0400

    Ejercicio 34

---
+++ src/34.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+
+
+int factorial(int n) {
+
+	if (n == 0) return 1;
+	int f = 1;
+
+	for (int i = n; i > 0; i--)
+		f = f * (i);
+
+    return f;
+}
+
+int is_curious(int n){
+
+	if ((n == 1) || (n == 2) || (n == 0)) return 0;
+
+	int aux = n,d,sum=0;
+
+	while (aux != 0) {
+		d = aux % 10;
+		sum += factorial(d);
+		aux /= 10;
+	 }
+
+	if (n == sum) return 1;
+	return 0;
+}
+
+
+int main() {
+
+	int sum = 0;
+
+	for (int i = 0; i < 50000; i++)
+		if (is_curious(i))
+			sum += i;
+	
+
+	printf("%i\n", sum);
+	system("pause");
+	return 0;
+}
