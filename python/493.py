5f44f8fc5401616e95115e69ec3bc8e39a87b335
cesarbonadio <cesarbonadio123@gmail.com>
2018-07-19 01:49:10 -0400

    ejercicio 493

---
+++ python/493.py
@@ -0,0 +1,33 @@
+def factorial(n):
+ if (n == 0) or (n == 1): 
+   return 1
+
+ result = 1	 
+
+ for i in range (1,n+1):
+ 	 result = result * i
+
+ return result
+
+
+def combinatoria(n1,n2):
+	return factorial(n1)/(factorial(n1-n2)*factorial(n2))
+
+
+#Este problema se resuelve usando combinatorias
+#
+#  Si se tiene una poblacion de 70 pelotas
+#  donde 20 son elegidas de una muestra
+#  10 para cada color 
+#
+#  entonces hay que encontrar la probabilidad de
+#  que NO salga un color en especifico, luego hallarle
+#  el complemento (1-p(x=0)) y por simetria multiplicar
+#  por la cantidad de colores que tenga el arcoiris
+#  7(1-(p(x=0)))
+#
+#  donde p(x=0) =  ((10c0)*(60c20))/(70c20)
+#  como 10c0 = 1
+#  entonces p(x=0) = (60c20)/(70c20)
+
+print(round(7*(1-(combinatoria(60,20)/combinatoria(70,20))),9))
