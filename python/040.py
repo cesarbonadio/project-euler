e04d2357991ca891d5d7f5dba5c0877f90e4c751
cesarbonadio <cesarbonadio123@gmail.com>
2018-08-21 15:44:03 -0400

    modificar enconding

--- python/040.py
+++ python/040.py
@@ -1,24 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 num=''
 
-#    185185 esel número de iteraciones de "i" que necesitamos concatenar para alcanzar un string
+#    185185 esel numero de iteraciones de "i" que necesitamos concatenar para alcanzar un string
 #    de longitud 1000000
 #
-#   -9 lugares para números de 1 dígito
-#   -180 lugares para números de 2 dígitos
-#   -2700 lugares para números de 3 dígitos
-#   -36000 lugares para números de 4 dígitos
-#   -450000 lugares para números de 5 dígitos
-#   -5400000 lugares para números de 6 dígitos pero
+#   -180 lugares para numeros de 2 digitos
+#   -2700 lugares para numeros de 3 digitos
+#   -36000 lugares para numeros de 4 digitos
+#   -450000 lugares para numeros de 5 digitos
+#   -5400000 lugares para numeros de 6 digitos pero
 #
 #    si necesitamos 1000000 y ya tenemos 9 + 180 + 2700 + 36000 + 450000 = 488889
 #
-#    de los 6 números de dígitos, solo necesitamos: (1000000-488889) /6≈85185.1666 ...
+#    de los 6 numeros de digitos, solo necesitamos: (1000000-488889) /6≈85185.1666 ...
 #
-#    El número máximo de 5 dígitos es 99999 + 85185.1666 ... = 185184.16666 ... ≈185185
+#    El numero maximo de 5 digitos es 99999 + 85185.1666 ... = 185184.16666 ... ≈185185
 #
-#    de esa manera, el último índice de la cadena sería exactamente 1000005
+#    de esa manera, el ultimo indice de la cadena seria exactamente 1000005
 #
-#    En realidad, dx1000000 es el primer dígito de 185185 , es decir, el 1
+#    En realidad, dx1000000 es el primer digito de 185185 , es decir, el 1
 #
 
 for i in range (185185+1):
